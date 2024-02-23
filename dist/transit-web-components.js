7f551f5d4acce79a05097f7d939033a8da45245b
Cesar Navarro Araya <cnaraya01@gmail.com>
2024-02-23 09:27:50 -0600

    Add Input component

--- dist/transit-web-components.js
+++ dist/transit-web-components.js
@@ -1,5 +1,5 @@
-import { openBlock as l, createElementBlock as a, normalizeClass as i, renderSlot as c } from "vue";
-const u = ["type"], d = {
+import { openBlock as s, createElementBlock as l, normalizeClass as c, renderSlot as b, toDisplayString as i, createCommentVNode as u, createElementVNode as g, mergeProps as p } from "vue";
+const y = ["type"], k = {
   __name: "Button",
   props: {
     type: { type: String, default: "button" },
@@ -10,24 +10,71 @@ const u = ["type"], d = {
   },
   setup(e) {
     const t = e;
-    function r() {
-      const o = t.secondary ? "text-secondary-700 hover:bg-secondary-700" : "text-primary-700 hover:bg-primary-700", n = t.secondary ? "bg-secondary-700 hover:bg-secondary-800" : "bg-primary-700 hover:bg-primary-800";
+    function a() {
+      const o = t.secondary ? "text-secondary-700 hover:bg-secondary-700" : "text-primary-700 hover:bg-primary-700", r = t.secondary ? "bg-secondary-700 hover:bg-secondary-800" : "bg-primary-700 hover:bg-primary-800";
       return [
         "font-medium rounded text-sm px-5 py-2.5 me-2 mb-2",
-        !t.outline && `text-white ${n}`,
+        !t.outline && `text-white ${r}`,
         t.outline && `bg-transparent outline outline-1 hover:text-white ${o}`,
         t.class
       ];
     }
-    return (o, n) => (l(), a("button", {
-      class: i(r()),
-      onClick: n[0] || (n[0] = (...s) => e.onClick && e.onClick(...s)),
+    return (o, r) => (s(), l("button", {
+      class: c(a()),
+      onClick: r[0] || (r[0] = (...n) => e.onClick && e.onClick(...n)),
       type: e.type
     }, [
-      c(o.$slots, "default")
-    ], 10, u));
+      b(o.$slots, "default")
+    ], 10, y));
+  }
+}, f = ["for"], x = {
+  key: 1,
+  class: "mt-2 text-sm text-red-600 dark:text-red-500"
+}, B = {
+  __name: "Input",
+  props: {
+    label: String,
+    id: String,
+    name: String,
+    type: String,
+    class: String,
+    wrapperClass: String,
+    error: String,
+    required: Boolean,
+    placeholder: String,
+    labelClass: String,
+    maxlength: Number,
+    pattern: String
+  },
+  setup(e) {
+    const t = e;
+    function a() {
+      const { label: n, wrapperClass: d, error: C, labelClass: S, ...m } = t;
+      return m;
+    }
+    function o() {
+      const n = "border text-sm rounded-lg block w-full p-2.5";
+      return t.error ? `${n} bg-red-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 outline-red-500` : `${n} border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500`;
+    }
+    function r() {
+      return t.error ? "block mb-1 text-sm font-medium text-red-700 dark:text-red-500" : "block mb-1 text-sm font-medium text-gray-900";
+    }
+    return (n, d) => (s(), l("div", {
+      class: c(e.wrapperClass)
+    }, [
+      e.label ? (s(), l("label", {
+        key: 0,
+        for: e.name,
+        class: c(r())
+      }, i(e.label), 11, f)) : u("", !0),
+      g("input", p({
+        class: o()
+      }, a()), null, 16),
+      e.error ? (s(), l("p", x, i(e.error), 1)) : u("", !0)
+    ], 2));
   }
 };
 export {
-  d as Button
+  k as Button,
+  B as Input
 };
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-7">
-    <h1>Buttons</h1>
+  <div class="p-7 pb-0">
+    <h3 class="mb-4 text-2xl font-extrabold leading-none text-gray-900">Buttons</h3>
     <div class="mt-3">
       <Button @click="clicked">Primary</Button>
       <Button secondary>Secondary</Button>
@@ -9,10 +9,31 @@
     </div>
     <hr>
   </div>
+  <div class="p-7 pb-0">
+    <h3 class="mb-4 text-2xl font-extrabold leading-none text-gray-900">Text Inputs</h3>
+    <div class="mt-3">
+      <div class="mb-2">Input with Label</div>
+      <Input label="Input label" id="text1" name="user[name]" />
+    </div>
+    <div class="mt-3">
+      <div class="mb-2">Input without Label</div>
+      <Input />
+    </div>
+    <div class="mt-3">
+      <div class="mb-2">Input Placeholder</div>
+      <Input placeholder="Some placeholder text"/>
+    </div>
+    <div class="mt-3">
+      <div class="mb-2">Input with error</div>
+      <Input label="Input label error" error="Error message" />
+    </div>
+    <hr>
+  </div>
 </template>
 
 <script setup>
 import Button from "./components/Button.vue"
+import Input from "./components/Input.vue"
 
 function clicked() {
   alert("Button clicked!")
