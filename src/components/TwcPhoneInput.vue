e0db00ab6bfe2d7e5d04e65ee5bbae6a4555f821
Cesar Navarro Araya <cnaraya01@gmail.com>
2025-07-23 10:26:52 -0600

    Libraries upgrades and security fixes

--- src/components/TwcPhoneInput.vue
+++ src/components/TwcPhoneInput.vue
@@ -89,7 +89,7 @@ onMounted(() => {
     initialCountry: "auto",
     geoIpLookup: getIp,
     autoPlaceholder: 'off',
-    utilsScript: process.env.NODE_ENV == 'test' ? '' : 'https://cdn.jsdelivr.net/npm/intl-tel-input@24.4.0/build/js/utils.js'
+    loadUtils: () => import('intl-tel-input/utils')
   })
 })
 </script>
